0718ac1b6a491c6c9822c37b416a51254d8847e5
Chiz1445 <bit22-ukanyimbo@mubas.ac.mw>
2026-09-19 09:14:44 +0200

    feat: point frontend to live Render backend

--- Frontend/src/API/Axios.js
+++ Frontend/src/API/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://10.143.119.136:3000/api',
+  baseURL: 'https://contactless-checkout-backend.onrender.com/api',
 })
 
 export default api
